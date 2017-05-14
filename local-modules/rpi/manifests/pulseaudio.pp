74cf5fae040521382055be77fec12659557fb187
Matus Goljer <matus.goljer@gmail.com>
2017-05-14 12:37:14 +0000

    Enable pulseaudio network sink

--- local-modules/rpi/manifests/pulseaudio.pp
+++ local-modules/rpi/manifests/pulseaudio.pp
@@ -1,11 +1,49 @@
 class pulseaudio {
 
   package { 'pulseaudio': ensure => installed }
+  package { 'pulseaudio-module-zeroconf': ensure => installed }
+  package { 'avahi-daemon': ensure => installed }
 
-  file_line { '/etc/pulse/default.pa':
+  file_line { '/etc/pulse/default.pa/module-native-protocol-tcp':
     path => '/etc/pulse/default.pa',
-    line => 'load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1',
+    match => '^load-module\ module-native-protocol-tcp',
+    line => 'load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1;192.168.10.0/24;10.0.0.0/24',
     require => Package['pulseaudio'],
+  }
+
+  file_line { '/etc/pulse/default.pa/module-zeroconf-publish':
+    path => '/etc/pulse/default.pa',
+    line => 'load-module module-zeroconf-publish',
+    require => Package['pulseaudio'],
+  }
+
+  # https://www.raspberrypi.org/forums/viewtopic.php?f=38&t=11124
+  file_line { '/etc/modules':
+    path => '/etc/modules',
+    line => 'snd_bcm2835',
+  }
+
+
+  file { '/etc/systemd/system/pulseaudio.service':
+    source => 'puppet:///modules/rpi/pulseaudio/pulseaudio.service',
+    owner => root,
+    group => root,
+    mode => 0644,
+    ensure => present,
+    require => Package['pulseaudio'],
+  }
+
+  service { 'pulseaudio':
+    enable => true,
+    ensure => running,
+    provider => systemd,
+    require => File['/etc/systemd/system/pulseaudio.service'],
+  }
+
+  service { 'avahi-daemon':
+    enable => true,
+    ensure => running,
+    require => Package['avahi-daemon'],
   }
 
 }
